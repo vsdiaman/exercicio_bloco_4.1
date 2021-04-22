3d674f2a73fc112eabccb8e19c944260c1f2c3a2
VitorDiamantino <vitor.diamantino8@gmail.com>
2021-04-22 18:02:18 -0300

    Retornando o numero maior da questao 2

--- quest1/questao_2.js
+++ quest1/questao_2.js
@@ -1,10 +1,10 @@
-let num1 = 3;
+let num1 = 32;
 let num2 = 12;
 
 if (num1 > num2) {
-    console.log("O numero 1 é maior que numero 2.");
+    console.log("O numero maior é " + num1);
 }else if (num1 < num2) {
-    console.log("O numero 2 é maior que o numero 1.");
+    console.log("O numero maior é " + num2);
 }else {
     console.log("Numeros iguais, digite um valor diferente.");
 } 

3955699aca53c8730ccbc940be8ceb72e3436d42
VitorDiamantino <vitor.diamantino8@gmail.com>
2021-04-22 19:38:03 -0300

    questao 5

---
+++ quest1/questao_5.js
@@ -0,0 +1,11 @@
+let ladoEsquerdo = 30;
+let ladoDireito2 = 30;
+let base3 = 120;
+
+let angulo = (ladoEsquerdo + ladoDireito2 + base3)
+
+if(angulo == 180) {
+    console.log("True")
+}else{
+    console.log(false)
+}

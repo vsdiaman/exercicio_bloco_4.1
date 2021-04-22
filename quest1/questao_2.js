b8dd463c286df94fa57c3bfd4fbf75401a497c5f
VitorDiamantino <vitor.diamantino8@gmail.com>
2021-04-22 17:51:21 -0300

    questao 2

---
+++ quest1/questao_2.js
@@ -0,0 +1,11 @@
+let num1 = 3;
+let num2 = 12;
+
+if (num1 > num2) {
+    console.log("O numero 1 é maior que numero 2.");
+}else if (num1 < num2) {
+    console.log("O numero 2 é maior que o numero 1.");
+}else {
+    console.log("Numeros iguais, digite um valor diferente.");
+} 
+
